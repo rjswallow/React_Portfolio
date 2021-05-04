2f76508045fa4eccc1074df34c4a534ebccccd7b
Ralds Oril <robertjswallow@gmail.com>
2021-05-04 00:00:19 -0700

    updated images  annd pdf  of  resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -4,7 +4,7 @@ export default function Resume() {
     return (
         <div className= "row d-flex justify-content-center mt-5">
             <div>
-                <a href= {process.env.PUBLIC_URL + "/images/projects/techres.pdf"} download> 
+                <a href= {process.env.PUBLIC_URL + "/images/projects/rswallowresume.pdf"} download> 
                     <img src={process.env.PUBLIC_URL + "/images/projects/resp1.png"} alt="resume page 1" />
                     <br />
                     <img src={process.env.PUBLIC_URL + "/images/projects/resp2.png"} alt="resume page 2" />
