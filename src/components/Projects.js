8fe5bf5c9f9d4b4c5fd461b70c852257d2535665
Ralds Oril <robertjswallow@gmail.com>
2021-05-04 14:16:48 -0700

    refactoring projects cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,14 +1,18 @@
 import project from "../projects.json";
 import  "../../src/index.css"
 import React, {Component} from "react"
-import {Button} from 'react-bootstrap'
+import {Button, Container, Grid, Row, Col} from 'react-bootstrap'
 import Card from "react-bootstrap/Card";
 import CardDeck from "react-bootstrap/Card";
 
+
 function Projects(){
     const SingleProject = project.project.map((item) =>
+    <Container ClassName= "container-fluid"> 
+    <Row ClassName= "row mb-5">
+    <Col ClassName= "col py-1 d-flex align-items-stretch">  
   <CardDeck>
- <Card style={{ height: "20%" }} ClassName= "cards flex" >
+ <Card style={{ height: "20%" }} ClassName= "col py-1 d-flex align-items-stretch" >
     <Card.Img variant="top" src={process.env.PUBLIC_URL + item.thumbnail}/>
     <Card.Body>
       <Card.Title> {item.title} </Card.Title>
@@ -21,6 +25,9 @@ function Projects(){
     </Card.Footer>
   </Card>
   </CardDeck>
+  </Col> 
+  </Row>
+  </Container>
     )
 
 return (
